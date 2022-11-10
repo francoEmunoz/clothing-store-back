edfe467a68405c9488a928e78e301dc674c495c1
francoEmunoz <munozfranco011@gmail.com>
2022-11-09 22:20:07 -0300

    unifico rutas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const productRouter = require('./products')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/products', productRouter)
 
 module.exports = router;
