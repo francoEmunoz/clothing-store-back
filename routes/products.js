f1c4bcf0183492ebbba529867ba5466e496eacf7
francoEmunoz <munozfranco011@gmail.com>
2022-11-16 22:22:33 -0300

    passport

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let passport = require("../config/passport");
 const { create, readAll, readOne, update, destroy } = require('../controllers/productController')
 
 router.post('/', create)
 router.get('/', readAll)
 router.get('/:id', readOne)
-router.patch('/:id', update)
-router.delete('/:id', destroy)
+router.patch('/:id', passport.authenticate("jwt", { session: false }), update)
+router.delete('/:id', passport.authenticate("jwt", { session: false }), destroy)
 
 module.exports = router;
